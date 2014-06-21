c93c78b0d34d2e2838c033c4c205b451eebb013a
subhra <s.shankha.roy@gmail.com>
2014-06-21 16:00:33 +0530

    submitting assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,12 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Write a short comment describing this function -makecachematrix take a matrix
+#  return a list of 4 items
+
 
   makeCacheMatrix <- function(x = matrix()) {
-    cachevalue<-NULL
+    cachevalue<-NULL      
     set<-function(y){
       x<<-y
       cachevalue<<-NULL
@@ -22,15 +24,15 @@
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-    cachevalue <- x$getmatrix()
-    if(!is.null(cachevalue)) {
+    cachevalue <- x$getmatrix()   #checking the X vectors cache
+    if(!is.null(cachevalue)) {    #if there is cache
       message("getting cached data")
-      return(cachevalue)
+      return(cachevalue)          # return the cachevalue no need to compute
     }
-    data <- x$get()
-    cachevalue <- solve(data, ...)
+    data <- x$get()               # if no cache is there
+    cachevalue <- solve(data, ...)  # compute the inverse
     x$setmatrix(cachevalue)
-    cachevalue
+    cachevalue                  #returning the inverse matrix of provided matrix 
   }
 #to test initialize a matrix called m
   #then follow-----
